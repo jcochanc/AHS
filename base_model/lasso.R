4d469e88bd9ecd06c57f6a46aa744f54dce44501
Linde Chen <lindechen@gmail.com>
2019-05-08 13:46:33 -0400

    perform variable selection with LASSO

--- base_model/lasso.R
+++ base_model/lasso.R
@@ -1,48 +1,36 @@
-library(glmnet) # load 'glmnet' package
+library(glmnet)
 library(dplyr)
+library(tidyr)
 
-data <- readRDS("/Users/lindechen/AHS/Data/df_two_indeces_demographic_vars.RDS") # read index and demographics data provided by Carolina
+data <- readRDS("/Users/lindechen/AHS/Data/train.RDS") 
 
 data <- data[,-1] # remove 'control', an ID variable
 data$sex <- data$sex-1 # make 'sex' a 0/1 binary variable
 data$data_year <- data$data_year-1997 # normalize 'data_year' to start at 1997
 data$weight <- (data$weight - mean(data$weight)) / sd(data$weight) # standardize `weight`
+data <- data[-which(is.na(data$housing_qual_index_alternative)),] # remove 2460 rows with NA's
 
-######################### perform LASSO regression on demographic variables #####################
-# Note: LASSO inherently performs variable selection by shrinking some coefficients to zero
+# perform LASSO regression on demographic variables 
+# LASSO inherently performs variable selection by shrinking some coefficients to zero
 
-######## 1. try with 'grad' (see codebook) - CONTINOUS outcome variable
-x <- as.matrix(data[,-c(5,6,24,25,26,27,28,29,30)]) # Removes class, educ_cat (redundant), dimension vars, index (both non-demographic)
-y <- as.double(as.matrix(data[, 5])) # Only class
+# use educ_cat as outcome variable (factor of 4 levels)
+features <- data %>% select(-grad, -educ_cat, -housing_qual_index, -housing_qual_index_alternative) # remove class, grad (redundant), indices 
+x <- as.matrix(features)
+y <- as.matrix(as.numeric(data[,"educ_cat"])) # only class
 
-# Fitting the model (Lasso: alpha = 1)
-set.seed(14)
-cv.lasso <- cv.glmnet(x, y, alpha=1, parallel=TRUE, standardize=TRUE, type.measure='mse')
+# Fit the model
+set.seed(67)
+fit <- glmnet(x, y, alpha=1, family = "multinomial")
+plot(fit)
+
+cv.lasso <- cv.glmnet(x, y, alpha=1, family = "multinomial", type.measure = "deviance")
 
 # Results
 plot(cv.lasso)
 plot(cv.lasso$glmnet.fit, xvar="lambda", label=TRUE)
 
 # all coef
-coef <- coef(cv.lasso, s=cv.lasso$lambda.1se)[,1] # add 1 SE to obtain more parsimonious model
-# non-zero demo. coef
-nonzero_coef_grad <- data.frame(coef = coef[which(coef(cv.lasso, s=cv.lasso$lambda.1se) != 0)])  
-
-saveRDS(nonzero_coef_grad, "./Data/nonzero_coef_grad.RDS")
-
-######## 2. try with `educ_cat` - MULTINOMIAL outcome variable
-x <- as.matrix(data[,-c(5,6,24,25,26,27,28,29,30)]) # Removes class, grad (redundant), dimension vars, index (both non-demographic)
-y <- as.matrix(as.numeric(data[, 6])) 
-
-# Fitting the model
-cv.lasso <- cv.glmnet(x, y, family = "multinomial", alpha=1, type.measure = "deviance")
-
-# Results
-plot(cv.lasso)
-plot(cv.lasso$glmnet.fit, xvar="lambda", label=TRUE)
-
-# all coefficients
-coef <- do.call(cbind, coef(cv.lasso, s = cv.lasso$lambda.1se)) 
+coef <- do.call(cbind, coef(cv.lasso, s = cv.lasso$lambda.1se)) # add 1 SE to obtain more parsimonious model
 
 index <- c() # create empty vector to store var. with coefficients in any of 4 categories shrunk to zero
 for (i in 1:nrow(coef)) {
@@ -53,4 +41,5 @@ for (i in 1:nrow(coef)) {
 
 # non-zero demo. coef
 nonzero_coef_educ_cat <- coef[-index,] 
-saveRDS(nonzero_coef_educ_cat, "./Data/nonzero_coef_educ_cat.RDS")
+
+saveRDS(nonzero_coef_educ_cat, "./Data/nonzero_coef.RDS")
